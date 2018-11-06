c8b0a920cf16443d53c141cfd065016783fd4c18
Akira Murayama <funya.akira@gmail.com>
2018-11-06 23:23:10 +0900

    Add New

---
+++ boards/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Board
+from django.contrib.auth.models import User
+from .models import Board, Topic, Post
+from django.http import Http404, HttpResponse
+from .forms import NewTopicForm, MemberModelForm
+from django.views.decorators.http import require_POST
+
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'boards/home.html',{'boards': boards})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'boards/topics.html', {'board': board})
+
+def new_topic(request,pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()
+
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+        # subject = request.POST['subject']
+        # message = request.POST['message']
+        #
+        # user = User.objects.first()
+        #
+        # topic = Topic.objects.create(
+        #     subject=subject,
+        #     board=board,
+        #     starter=user
+        # )
+        #
+        # post = Post.objects.create(
+        #     message=message,
+        #     topic=topic,
+        #     created_by=user
+        # )
+
+            return redirect('boards:board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
+
+    return render(request, 'boards/new_topic.html', {'board': board, 'form': form})
+
+
+
+def e1_index(request):
+    context = {
+        'form': MemberModelForm(),
+    }
+    return render(request, 'boards/e1/index.html', context)
+
+@require_POST
+def e1_save(request):
+    form = MemberModelForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('boards:e1_index')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'boards/e1/index.html', context)
