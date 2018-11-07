a5cb59cc8c593689a3b049cf2ff924acefe9367c
Akira Murayama <funya.akira@gmail.com>
2018-11-07 21:21:54 +0900

    Edit

--- accounts/tests.py
+++ accounts/tests.py
@@ -3,12 +3,19 @@ from django.test import TestCase
 from.views import signup
 
 class SignUpTests(TestCase):
-    def test_signup_status_code(self):
+    def setUp(self):
         url = reverse('signup')
-        print(url)
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.response = self.client.get(url)
+
+    def test_signup_status_code(self):
+        self.assertEquals(self.rsponse.status_code, 200)
 
     def test_signup_url_resolves_signup_view(self):
         view = resolve('/signup/')
         self.assertEquals(view.func, signup)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csfmiddlewaretoken')
+
+    def test_contains_form(self):
+        
